ddd05a5669a35a21dd9c3d3c0eb43bcdc3c66b18
Emmanuella Nana Yaa Adjei <eladyaa15@gmail.com>
2025-09-17 12:26:53 +0000

    Updated files

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ExampleForm
 from django.contrib.auth import login
 
 # Http Response
